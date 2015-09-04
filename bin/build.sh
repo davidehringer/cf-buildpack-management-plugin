cd3d419d40d3aecacce563567aef01c6e06504f6
dehringer <david.ehringer@gmail.com>
2015-09-03 23:26:27 -0400

    Added build and release scripts

---
+++ bin/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ "$(which gox)X" == "X" ]]; then
+  echo "Please install gox. https://github.com/mitchellh/gox#readme"
+  exit 1
+fi
+
+
+rm -f cf-buildpack-management-plugin*
+
+gox -os linux -os windows -arch 386
+gox -os darwin -os linux -os windows -arch amd64
+
+rm -rf out
+mkdir -p out
+mv cf-buildpack-management-plugin* out/
